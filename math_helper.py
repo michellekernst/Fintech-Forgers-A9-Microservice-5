deb4e9e11edada74efc9299985d08803bf35a2d1
michellekernst <mke.ernst@gmail.com>
2026-08-10 11:50:35 -0500

    Refactor calculate function for better clarity
    Refactor calculate function to improve readability.

--- math_helper.py
+++ math_helper.py
@@ -6,17 +6,20 @@ response_file = "math_response.txt"
 
 
 def calculate(operation, *numbers):
+
     # clear the old response
     open(response_file, "w").close()
 
     # format the request
-    request = operation + " " + " ".join(str(number) for number in numbers)
+    request = operation + " " + " ".join(
+        str(number) for number in numbers
+    )
 
     # send the request
     with open(request_file, "w") as f:
         f.write(request)
 
-    # wait for a new response
+    # wait for the response
     while True:
         with open(response_file) as f:
             response = f.read().strip()
